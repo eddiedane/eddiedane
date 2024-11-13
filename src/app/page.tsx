424e3ed99d3c0dff533924e45b34df38b295a3c5
Eddie Dane <eddiedane@ymail.com>
2024-11-13 21:03:19 +0100

    style: Add intro animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import Skills from '@/components/sections/skills';
 import Education from '@/components/sections/education';
 import About from '@/components/sections/about';
 import Footer from '@/components/layout/footer';
-
+import Pizazz from '@/components/pizazz';
 export default function Home() {
   return (
     <>
       <main>
+        <Pizazz />
         <Intro />
         <Projects />
         <Skills />
